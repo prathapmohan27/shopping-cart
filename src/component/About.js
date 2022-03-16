14f5cf0b7487658fb058a7b3d809f48c2c420606
prathapmohan27 <prathapmohan2704@gmail.com>
2022-03-16 07:35:18 +0530

    Add about component

--- src/component/About.js
+++ src/component/About.js
@@ -1,8 +1,49 @@
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
+
+const Div = styled.div`
+  width: 50rem;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 10rem;
+  h1 {
+    text-align: center;
+    font-size: 2.5rem;
+  }
+  div {
+    display: flex;
+    justify-content: space-between;
+    margin: 1rem;
+  }
+`;
+
+const A = styled.a`
+  border-radius: 50%;
+  color: #484a48;
+  font-size: 10rem;
+`;
+
 function About() {
   return (
-    <div>
-      <h1>About page</h1>
-    </div>
+    <Div>
+      <h1>About Us</h1>
+      <div>
+        <A href="https://github.com/prathapmohan27" target="_blank">
+          <FontAwesomeIcon icon={faGithub} />
+        </A>
+        <A href="mailto:prathapmohan2704@gmail.com" target="_blank">
+          <FontAwesomeIcon icon={faPaperPlane} />
+        </A>
+        <A
+          href="https://www.linkedin.com/in/prathap-m-b104101b8/"
+          target="_blank"
+        >
+          <FontAwesomeIcon icon={faLinkedin} />
+        </A>
+      </div>
+    </Div>
   );
 }
 
